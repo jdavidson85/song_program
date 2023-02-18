1afaa7fee0761fba92e3ea1ab1cd71d976b5d9b8
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-02-18 13:53:41 -0600

    Add files via upload

---
+++ song_beer_on_the_wall.py
@@ -0,0 +1,19 @@
+bottles = 99
+while bottles > 2:
+    print(str(bottles) + " bottles of beer on the wall,")
+    print(str(bottles) + " bottles of beer,")
+    print("take one down, pass it around,")
+    print(str(bottles - 1) + " bottles of beer on the wall.\n\n")
+    bottles = -1
+
+print(str(bottles) + " bottles of beer on the wall,")
+print(str(bottles) + " bottles of beer,")
+print("take one down, pass it around,")
+print(str(bottles - 1) + " bottle of beer on the wall.\n\n")
+bottles = -1
+
+print(str(bottles) + " bottle of beer on the wall,")
+print(str(bottles) + " bottle of beer,")
+print("take one down, pass it around,")
+bottles = -1
+print(str(bottles) + " bottles of beer on the wall")
